68a4d4d88f02757605ab62d66ef5f1fd09f88f9a
ADem <ademflipper@gmail.com>
2025-11-17 23:49:43 +0100

    v1.00

---
+++ functions.js
@@ -0,0 +1,9 @@
+function namesToUpperCase(names) {
+    const counter = 5;
+    while (counter > 0) {
+        counter--;
+    }
+    return namesToUpperCase();
+}
+const names = ["adam", "eve", "steve"];
+console.log(namesToUpperCase(names)); 
---
+++ index.c
@@ -0,0 +1,11 @@
+int i = 1;
+int counter = 0;
+void main() {
+    for (i = 1; i <= 5; i++) {
+        counter += i;
+    }
+    printf("Final counter value %d\n", counter);
+    for (int n = 1; n = 5; n++) {
+        printf("%d\n", n + i);
+    }
+}
